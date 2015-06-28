9b5c62c184098ef28f7e64e083e4eb06470d2f4e
renoji5126 <renoji5126@gmail.com>
2015-06-28 18:29:16 +0900

    クラスメソッド、クラス変数

--- src/class.rb
+++ src/class.rb
@@ -1,17 +1,26 @@
 #
 # class名は必ず頭大文字で。
 class User
+  @@count = 0
   def initialize(name)
     @name = name
+    @@count += 1
   end
 
-  def sayHi
+  def sayHi # インスタンスメソッド
     puts "hello, my name is #{@name}"
   end
+  
+  def User.sayHello # クラスメソッド
+    puts "hello from User class (#{@@count} users)"
+  end
 end
+
+User.sayHello()
 
 tom = User.new("Tom")
 bob = User.new("Bob")
 
 tom.sayHi
 bob.sayHi
+User.sayHello()
